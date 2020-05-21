fa0ea8ca09de26f08d4ef4250ba14ffb239fb726
jmbarley1 <60192277+jmbarley1@users.noreply.github.com>
2020-05-21 16:35:06 -0400

    CPUE EDA and Analysis code

---
+++ 02_CPUE_EDA_analysis.R
@@ -0,0 +1,114 @@
+##Catch per unit effort (CPUE) Data exploration and analysis
+#Jordanna Barley
+#Updated May 21, 2020
+
+require(glmmTMB)
+require(AICcmodavg)
+require(pscl)
+require(MASS)
+require(MuMIn)
+require(car)
+require(ggplot2)
+require(lme4)
+require(emmeans)
+require(brglm)
+require(multcomp)
+require(viridis)
+require(dplyr)
+require(pscl)
+require(car)
+
+#reading in clean data
+cpue<-read.csv('CBOR_cpue_clean_2020.csv')
+
+#Initial Look----------------------------------------------------------------
+#An initial look at the data shows that many of the counts that are less than 30 are members of our team:
+out<-filter(cpue, drills.caught<25)
+out<-data.frame(
+  'Fisher'=out$Fisher,
+  'drills.caught'=out$drills.caught,
+  'CPUE'=out$CPUE
+)
+out
+#Therefore, those data points were not included in the analysis
+cpue<-filter(cpue, drills.caught>25)
+
+#making sure that data is in the right format
+str(cpue)
+cpue$year<-factor(cpue$year)
+
+
+#Data exploration-----------------------------------------------------------
+
+cpue.plot<-ggplot(cpue, aes(x=Date, y=CPUE)) #initial look at barchart of CPUE
+cpue.plot+geom_bar(stat='identity')+
+  theme(axis.text.x = element_text(angle = 45, hjust=1))+
+  facet_wrap(~Site)+
+  ylab('Catch per unit effort')
+
+cpue.plot2<-ggplot(cpue, aes(x=day_of_year, y=CPUE, color=year)) #looking at CPUE across year and site
+cpue.plot2+geom_point(alpha=0.4, size=2)+
+  stat_summary(fun.args = list(mult=1),geom='pointrange', size=0.75)+
+  facet_wrap(~Site)+
+  theme_linedraw()+
+  theme(text=element_text(size=20))+
+  ylab('Catch per Unit Effort')+
+  xlab('Day of Year')
+
+
+#Models----------------------------------------------------------------------
+
+#started with CLMs with poisson errors
+cpue.null<-glm(drills.caught~1, data = cpue, offset = Person.hrs, family = 'poisson')
+cpue.moda<-glm(drills.caught~day_of_year, data=cpue, offset = Person.hrs, family = 'poisson')
+cpue.modb<-glm(drills.caught~year, data=cpue, offset = Person.hrs, family = 'poisson')
+cpue.modc<-glm(drills.caught~day_of_year+year, data = cpue, offset = Person.hrs, family = 'poisson')
+cpue.modd<-glm(drills.caught~day_of_year*year, data = cpue, offset = Person.hrs, family = 'poisson')
+cpue.mode<-glm(drills.caught~Site, data = cpue, offset = Person.hrs, family = 'poisson')
+cpue.modf<-glm(drills.caught~Site+day_of_year+year, data = cpue, offset = Person.hrs, family = 'poisson')
+cpue.modg<-glm(drills.caught~Site+day_of_year*year, data = cpue, offset = Person.hrs, family = 'poisson') #top model
+model.sel(cpue.null, cpue.moda, cpue.modb, cpue.modc, cpue.modd, cpue.mode, cpue.modf, cpue.modg)
+
+#Overdispersion check on top model
+dev<-summary(cpue.modg)$deviance
+dof<-summary(cpue.modg)$df.residual
+1-pchisq(dev,dof)#chi-squared test; want >0.05
+dev/dof #dispersion parameter; want ~1
+#these tests indicate that the top model is overdispersed
+
+#looking at Negative binomial GLMs and GLMMs
+
+cpue.null<-glm.nb(drills.caught~1, data = cpue, offset(Person.hrs))
+cpue.moda<-glm.nb(drills.caught~day_of_year, data=cpue, offset(Person.hrs))
+cpue.modb<-glm.nb(drills.caught~year, data=cpue, offset(Person.hrs))
+cpue.modc<-glm.nb(drills.caught~day_of_year+year, data = cpue, offset(Person.hrs))
+cpue.modd<-glm.nb(drills.caught~day_of_year*year, data = cpue, offset(Person.hrs))
+cpue.mode<-glm.nb(drills.caught~Site, data = cpue, offset(Person.hrs))
+cpue.modf<-glm.nb(drills.caught~Site+day_of_year+year, data = cpue,  offset(Person.hrs))
+cpue.modg<-glm.nb(drills.caught~Site+day_of_year*year, data = cpue,  offset(Person.hrs))
+cpue.glmm<-glmmTMB(drills.caught~day_of_year+(1|Site)+(1|year), data=cpue, offset=Person.hrs, family = nbinom1)
+cpue.glmm2<-glmmTMB(drills.caught~day_of_year+(1|Site)+(1|year), data=cpue, offset=Person.hrs, family = nbinom2)
+#cpue.glmm3<-glmmTMB(drills.caught~day_of_year*year+(1|Site)+(1|year), data=cpue, offset=Person.hrs, family = nbinom1) error, not able to run
+cpue.glmm4<-glmmTMB(drills.caught~day_of_year*year+(1|Site)+(1|year), data=cpue, offset=Person.hrs, family = nbinom2)
+test.cpue<-glmmTMB(drills.caught~day_of_year*Site+(1|year), data=cpue, offset=Person.hrs, family = nbinom2) #model we are actually interested in
+test2.cpue<-glmmTMB(drills.caught~Site+(1|year), data=cpue, offset=Person.hrs, family = nbinom2) #AIC winner
+#model selection
+model.sel(test2.cpue, test.cpue,cpue.null, cpue.moda, 
+          cpue.modb, cpue.modc, cpue.modd, cpue.mode, 
+          cpue.modf, cpue.modg, cpue.glmm, cpue.glmm2, cpue.glmm4)
+
+summary(cpue.glmm2)
+summary(test.cpue)
+summary(test2.cpue)
+#none of these models show significant effects
+
+#looking at differences in site
+site1<-glm.nb(drills.caught~Site, offset(Person.hrs), data = cpue)
+summary(site1)
+#seems to be a difference in cpue across site
+
+
+
+
+
+
