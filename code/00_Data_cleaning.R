343c4a0f9dde0cdc59c00b8f50e2240bfc8cd1c4
jmbarley1 <60192277+jmbarley1@users.noreply.github.com>
2020-06-04 17:22:23 -0400

    Pushing drill size frequency data and EDA code

--- code/00_Data_cleaning.R
+++ code/00_Data_cleaning.R
@@ -9,7 +9,7 @@ require(dplyr)
 require(lubridate)
 
 #Growth/size-------------------------------------------------------
-size<-read.csv(here('CBOR_closedtreat_oystersize_original.csv'))
+size<-read.csv(here('data','CBOR_closedtreat_oystersize_original.csv'))
 
 #getting rid of the notes column
 size<-size[-c(13)]
@@ -24,12 +24,12 @@ write.csv(size, file = 'CBOR_closedtreat_oystersize_clean_2020.csv') #write data
 
 
 #Oyster drill abundance data-----------------------------------------
-drill<-read.csv(here('Benthic survey Richardson Bay COUNTS 11 Sept 2018 cjz.csv'))
+drill<-read.csv(here('data', 'Benthic survey Richardson Bay COUNTS 11 Sept 2018 cjz.csv'))
 
 drill$Year<-factor(drill$Year, levels = c('2017','2018')) #making sure that data is in the correct format
 
 #Catch per unit effort (CPUE)------------------------------------------------
-cpue<-read.csv(here('CBOR_cpue_original_2020.csv'))
+cpue<-read.csv(here('data','CBOR_cpue_original_2020.csv'))
 
 cpue<-select(cpue, -c(12:14)) #getting rid of extra columns
 cpue<-drop_na(cpue) #getting rid of NAs
@@ -51,3 +51,32 @@ cpue$Date<-droplevels(cpue$Date)
 #survival data- Brian's code------------------------------------------
 #data<-read.csv("Cage experiment recovery data 2018-08-13 RAW.csv")
 
+
+#Oyster Drill Size Frequency------------------------------------------
+data<-read.csv(here('data', 'CBOR_quadrat_OysterDrill_size.csv'))
+
+head(data)
+str(data)
+#making sure that data is in the right format
+data$Site<-as.factor(data$Site)
+data$year<-as.factor(data$year)
+
+#making a new column for treatment: control or eradication
+data$treatment[data$Site=='Aramburu Central']<-'eradication'
+data$treatment[data$Site=='Lanis Beach']<-'eradication'
+data$treatment[data$Site=='Lanis Boulders']<-'eradication'
+data$treatment[data$Site=='Aramburu South']<-'control'
+data$treatment[data$Site=='Aramburu South']<-'control'
+data$treatment[data$Site=='Blackies']<-'control'
+data$treatment[data$Site=='Bothin']<-'control'
+data$treatment[data$Site=='Brickyard Cove']<-'control'
+data$treatment[data$Site=='Cove Apts']<-'control'
+data$treatment[data$Site=='Cove']<-'control'
+data$treatment[data$Site=='Dunphy Park']<-'control'
+data$treatment[data$Site=='Hilarita']<-'control'
+data$treatment[data$Site=='pink House']<-'control'
+data$treatment<-as.factor(data$treatment) #making sure that treatment column is a factor
+str(data)
+sum(is.na(data$treatment)) #Making sure I got every row
+
+write.csv(data, file = 'CBOR_OysterDrill_sizefequency_clean_2020.csv')
---
+++ code/03_OysterDrillsizeFrequency_EDA_analysis.R
@@ -0,0 +1,65 @@
+##Efficacy of functional eradication over time (size frequency) EDA and analysis
+#Jordanna Barley
+#Updated 6/2/20
+
+require(here)
+require(tidyverse)
+
+data<-read.csv(here('data', 'CBOR_OysterDrill_sizefequency_clean_2020.csv'))
+
+#Plots----------------------------------------------------
+
+#histogram of distribution by site
+data %>% 
+  ggplot(aes(x=size))+
+  geom_histogram()+
+  facet_wrap(~Site) #there seems to be a bimodal distribution at almost every site
+
+data %>% 
+  ggplot(aes(x=size))+
+  geom_histogram()+
+  facet_wrap(~treatment) #same bimodal distribution
+
+data %>% 
+  ggplot(aes(x=day_of_year, y=size))+
+  geom_point(alpha=0.5, position = position_jitter())+
+  stat_summary(fun.args = list(mult=1),geom='pointrange', size=0.75, col='red')+
+  facet_wrap(~year)+
+  ylab('Oyster drill size')+
+  xlab('Day of Year') #2017: can maybe see a decrease in oyster drill size
+
+data %>% 
+  ggplot(aes(x=day_of_year, y=size, color=year))+
+  geom_point(alpha=0.5, position = position_jitter())+
+  stat_summary(fun.args = list(mult=1),geom='pointrange', size=0.75)+
+  geom_smooth(method = 'lm')+
+  facet_wrap(~treatment)+
+  ylab('Oyster drill size (mm)')+
+  xlab('Day of Year') 
+
+data %>% 
+  ggplot(aes(x=day_of_year, y=size, color=treatment))+
+  geom_point(alpha=0.5, position=position_jitter())+
+  stat_summary(fun.args = list(mult=1), geom='pointrange', size=0.75)+
+  geom_smooth(method = 'lm')+
+  facet_wrap(~year)+
+  ylab('Oyster drill size (mm)')+
+  xlab('Day of Year') #it seems likely that size in 2017 did decrease, but not because of treatment. size at erdication site seems to have increased in 2018. 
+
+
+#Models------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
