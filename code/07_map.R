991c87dacb4a032f431144a38de8bf146506c7db
Brian Cheng <bscheng@ECO-184175.local>
2021-01-28 13:12:16 -0500

    plotting new shp files

--- code/07_map.R
+++ code/07_map.R
@@ -1,10 +1,12 @@
 
 library(rgdal)
+library(sf)
 library(ggplot2)
+library(ggsn)
 
 sfb<-st_read("data/map/bayarea_general.shp")
 
-st_geometry_type(sfb)
+tst_geometry_type(sfb)
 st_crs(sfb)             #check coordinate reference system
 st_bbox(sfb)            #find extent
 sfb                     #view all metadata
@@ -23,6 +25,22 @@ ggplot()+geom_sf(data=sfb, size=1, color="black")+       #sfb regional tweaked
 rb<-ggplot()+geom_sf(data=sfb, size=0.8, color="black", fill = "white")+
   coord_sf(crs = "NAD83", xlim = c(-122.53, -122.44), ylim = c(37.85, 37.9)) #richardson bay
 rb
+
+#new shp files from jeff
+castate<-st_read("data/map/jeff/CA_State_TIGER2016.shp")
+st_crs(castate)             #check coordinate reference system
+ggplot()+geom_sf(data=castate, size=1, color="black", fill = "white")+coord_sf(crs = "WGS84")  #map of state
+
+delta<-st_read("data/map/jeff/delta_Project.shp")
+st_crs(delta)             #check coordinate reference system
+ggplot()+geom_sf(data=delta, size=1, color="black", fill = "white")+coord_sf(crs = "WGS84")  
+
+
+ggplot()+geom_sf(data=delta, size=1, color="black", fill ="white")+       
+  coord_sf(crs = "WGS84", xlim = c(-122.53, -122.41), ylim = c(37.85,37.9))+  
+  theme_bw()+
+  theme(panel.background =  element_rect (fill="light grey"),
+  panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 
 #blog https://www.r-spatial.org/r/2018/10/25/ggplot2-sf-2.html
